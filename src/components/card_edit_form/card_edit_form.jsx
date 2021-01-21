67f48d15b01330080e41f2f3867285b6817ecdb1
sunghyun <amakasi87@gmail.com>
2021-01-21 17:30:41 +0900

    카드 프로필화면

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./card_edit_form.module.css";
+
+const CardEditForm = ({ card }) => {
+  return <h1>{card.name}</h1>;
+};
+
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./maker.module.css";
@@ -7,6 +7,38 @@ import Preview from "../preview/preview";
 import Editor from "../editor/editor";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "카즈야 미시마",
+      fightStyle: "미시마류 가라데",
+      country: "일본",
+      theme: "colorful",
+      skill: "초풍신, 나락",
+      fileName: "kazuya",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "화랑",
+      fightStyle: "태권도",
+      country: "한국",
+      theme: "light",
+      skill: "초스카이, 로하이",
+      fileName: "hwarang",
+      fileURL: "hwarang.png",
+    },
+    {
+      id: "3",
+      name: "킹",
+      fightStyle: "레슬링",
+      country: "멕시코",
+      theme: "dark",
+      skill: "연속잡기, 샤이닝",
+      fileName: "king",
+      fileURL: "king.png",
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -25,8 +57,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>

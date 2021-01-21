1861725626c5d9e8600ce83d4557c94cd9c07a93
sunghyun <amakasi87@gmail.com>
2021-01-21 18:16:22 +0900

    add editor form

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,8 +1,53 @@
 import React from "react";
+import Button from "../button/button";
+import ImageFileInput from "../image_file_input/image_file_input";
 import styles from "./card_edit_form.module.css";
 
 const CardEditForm = ({ card }) => {
-  return <h1>{card.name}</h1>;
+  const {
+    id,
+    name,
+    fightStyle,
+    country,
+    theme,
+    skill,
+    fileName,
+    fileURL,
+  } = card;
+
+  const onSubmit = () => {};
+
+  return (
+    <form className={styles.form}>
+      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        className={styles.input}
+        type="text"
+        name="fightStyle"
+        value={fightStyle}
+      />
+      <select name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="country"
+        value={country}
+      />
+      <textarea
+        className={styles.textarea}
+        name="skill"
+        value={skill}
+      ></textarea>
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit}></Button>
+    </form>
+  );
 };
 
 export default CardEditForm;
